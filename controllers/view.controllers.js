c4eea09349f6f259605e9935c1f026bd19ca0c64
Antonio Alvarez Mayoral <aalvarez59@ucol.mx>
2024-03-14 14:19:22 -0600

    Controlador que renderiza vistas

--- controllers/view.controllers.js
+++ controllers/view.controllers.js
@@ -1,23 +1,29 @@
-const { UserManager } = require("../services/user.services.js");
+const { userManager } = require("../services/user.services.js");
 
 const renderHome = ( req, res, next ) => {
     const rolPrivilege = res.locals.privilege;
 
     if( rolPrivilege === 2 ){
-        res.render('layouts/home', {
-            title: 'Admin Panel',
-            rol: 'admin'
-        });
+        res.redirect(302, 'admin/panel');
+        // res.render('layouts/home', {
+        //     title: 'Admin Panel',
+        //     rol: 'admin'
+        // });
 
     }else if( rolPrivilege === 1){
+        // res.send('you are a common user');
+        let access = true;
+        if(!res.locals.access) access = false;
         res.render('layouts/home', {
             title: 'Home',
-            rol: 'common_user'
+            rol: 'common_user',
+            'access': access
         });
     }else{
-        res.status(401).send({
-            msg: 'Your session is not valid!'
-        });
+        res.redirect( 302,'/user/login' );
+        // res.status(401).send({
+        //     msg: 'Your session is not valid!'
+        // });
     }
 }
 
