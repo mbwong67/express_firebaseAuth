f9ee546a6825cce693945a7563d8ddb26c7c842d
Antonio Alvarez Mayoral <aalvarez59@ucol.mx>
2024-03-14 14:29:45 -0600

    Config del firebase admin

--- helpers/firebase_admin.js
+++ helpers/firebase_admin.js
@@ -1,11 +1,21 @@
-const { initializeApp } = require('firebase-admin/app');
+const { initializeApp, getApps, getApp } = require('firebase-admin/app');
+const { admin, credential } = require('firebase-admin');
 const { SA_KEY_PATH, DATABASE_URL } = require('./config');
 const { getAuth } = require('firebase-admin/auth');
 
 
-const admin = initializeApp({
-    credential: admin.credential.cert(SA_KEY_PATH),
+// admin.app.initializeApp({
+//     credential: admin.credential.cert(SA_KEY_PATH),
+//     databaseURL: DATABASE_URL
+// });
+// const adminApp = initializeApp({
+//     credential: applicationDefault(),
+//     databaseURL: DATABASE_URL
+// });
+const adminCredentials = {
+    credential: credential.cert(SA_KEY_PATH),
     databaseURL: DATABASE_URL
-});
-const authAdmin = getAuth(admin);
+}
+const adminApp = getApps().length === 0 ? initializeApp(adminCredentials) : getApp();
+const authAdmin = getAuth(adminApp);
 module.exports = { authAdmin };
