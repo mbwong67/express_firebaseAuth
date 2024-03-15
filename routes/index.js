073fa399b33c6079829d5feb36db6f4f9d17f042
Antonio Alvarez Mayoral <aalvarez59@ucol.mx>
2024-03-14 18:30:13 -0600

    integración de nuevo router y vista principal

--- routes/index.js
+++ routes/index.js
@@ -1,4 +1,7 @@
-//TODO import specific routes
+const userMiddleware = require("../middleware/user.middleware.js");
+const viewController = require("../controllers/view.controllers.js");
+
+const adminRoute = require("./admin.js");
 const usuarioRoute = require("./users.js");
 
 const express = require('express'); //express package
@@ -10,10 +13,11 @@ router.use(express.json());
 
 
 // how 2 use router
-// router.use('/productos', productoRoute);
+router.use('/admin', adminRoute);
 router.use('/user', usuarioRoute);
 router.get('/', (req, res) =>{
     
     res.render('layouts/form');
 })
+router.get('/home', userMiddleware.isLoggedIn, viewController.renderHome);
 module.exports = router;
