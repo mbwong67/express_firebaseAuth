1d0d625f136f72092f988c30383b0fcd4cdf8020
Antonio Alvarez Mayoral <aalvarez59@ucol.mx>
2024-03-14 18:23:29 -0600

    Se integra ip para la url base y borrado de mensaje de error luego de un tiempo

--- public/scripts/SI_form.js
+++ public/scripts/SI_form.js
@@ -1,6 +1,6 @@
 // import { BASE_URL } from "../../helpers/config";
 const errorMessage = document.getElementById("errMessage");
-const errContainer = document.getElementById("errMessage");
+
 console.log('fuera dle submite');
 document.getElementById("SI_form").addEventListener("submit", (e) =>{
     e.preventDefault();
@@ -15,8 +15,10 @@ document.getElementById("SI_form").addEventListener("submit", (e) =>{
 
     // JS obj to JSON obj
     const jsonData = JSON.stringify(formDataObj);
-    const baseUrl = 'http://localhost:3000/user/';
-    fetch(baseUrl + "login", {
+
+    const ip = 'localhost'
+    const baseUrl = `http://${ip}:3000/`;
+    fetch(baseUrl + "user/login", {
         method: "POST",
         body: jsonData,
         headers: {
@@ -26,15 +28,13 @@ document.getElementById("SI_form").addEventListener("submit", (e) =>{
     .then( response => {
         if( response.status === 200 ){ //si es 200 redireccionar
             // return fetch(baseUrl + "login");
-            window.location.href = baseUrl + 'login';
+            window.location.href = baseUrl + 'home';
         }else{//sino, quedarse en la vista de sign up y mostrar mensaje de error
             e.preventDefault();
-            console.error('console');
-            const errorMessage = document.createElement('p');
             errorMessage.textContent = 'Login Error';
-            
-            errContainer.appendChild(errorMessage);
+            setTimeout(()=>{
+                errorMessage.textContent = '';
+            },3000);
         }
     })
-    console.log('despuesde');
 })
