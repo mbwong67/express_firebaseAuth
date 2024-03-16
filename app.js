bb922deb8475d29bd34deb82472714e2aae3c6e1
Antonio Alvarez Mayoral <aalvarez59@ucol.mx>
2024-03-16 00:21:00 -0600

    Ajuste del servidor para permitir el set-cookie

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const PORT = require("./helpers/config").PORT;
+const {PORT, BASE_URL} = require("./helpers/config");
 
 const express = require('express');
 const routes = require('./routes/index');
@@ -8,11 +8,16 @@ const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
-app.use(cors());
+
+app.use(cors({
+    origin: BASE_URL,
+    credentials: true
+}));
 
 app.use('/', routes); //to use the routes
 app.set('view engine', 'ejs');
-app.use(express.static("public"));
+app.use(express.static('public'));
+app.use(express.static('helpers'));
 
 const listener = app.listen(PORT || 3000, () => {
     console.log('Your app is listening on port ' + listener.address().port)
