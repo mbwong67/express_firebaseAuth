7f21f0d0967187c67868beafcc9bdd255d5101cc
Antonio Alvarez Mayoral <aalvarez59@ucol.mx>
2024-03-16 00:03:35 -0600

    Codigo comentado, futuros posibles cambios

--- controllers/view.controllers.js
+++ controllers/view.controllers.js
@@ -1,4 +1,6 @@
+//TODO: cehcar si no se necesita este obj
 const { userManager } = require("../services/user.services.js");
+const { cookieManager } = require('../services/cookie.services');
 
 const renderHome = ( req, res, next ) => {
     const rolPrivilege = res.locals.privilege;
@@ -12,12 +14,16 @@ const renderHome = ( req, res, next ) => {
 
     }else if( rolPrivilege === 1){
         // res.send('you are a common user');
-        let access = true;
-        if(!res.locals.access) access = false;
+        // let access = true;
+        // if(!res.locals.access) access = false;
+        // if( res.locals.newToken ){
+        //     const cookie = cookieManager.generateCookie(res.locals.newToken);
+        //     res.setHeader('Set-Cookie', cookie);
+        // }
         res.render('layouts/home', {
             title: 'Home',
             rol: 'common_user',
-            'access': access
+            'access': res.locals.access
         });
     }else{
         res.redirect( 302,'/user/login' );
