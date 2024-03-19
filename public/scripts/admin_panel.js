b9247ae8d3e2fd7b174398ff17f99a3b4c0dca90
Antonio Alvarez Mayoral <aalvarez59@ucol.mx>
2024-03-19 10:03:23 -0600

    Integracion del sign out

--- public/scripts/admin_panel.js
+++ public/scripts/admin_panel.js
@@ -1,13 +1,32 @@
 const checkboxes = document.querySelectorAll('input[type=checkbox]');
 const messageContainer = document.getElementById('messageResponse');
+const signOutForm = document.getElementById('signOut_form');
+
+
+signOutForm.addEventListener("submit", (e)=>{
+    e.preventDefault();
+    const ip = 'localhost'
+
+    const baseUrl = `http://${ip}:3000/user/`;
+    fetch(baseUrl + "sign-out", {
+        method: "POST",
+    })
+    .then( response => {
+
+        if( response.status === 200 || response.status === 400){ //si es 201 redireccionar
+            window.location.href = baseUrl + 'login';
+        }else{//sino, quedarse en la vista de sign up y mostrar mensaje de error
+            errorMessage.textContent = 'Error in process sign out';
+        
+        }
+    });
+});
+
 
 let programmaticChange = false;
-console.log('caca')
-console.log(checkboxes)
 // Iterate all checkboxes
 let i = 0;
 checkboxes.forEach(checkbox => {
-    console.log(i)
   checkbox.addEventListener('change', function(e) {
     if( !programmaticChange ){
         e.preventDefault();
--- public/scripts/user_home.js
+++ public/scripts/user_home.js
@@ -1,23 +1,21 @@
-const { onAuthStateChanged } = require('../../helpers/firebase');
+const signOutForm = document.getElementById('signOut_form');
 
-const unsubscribe = onAuthStateChanged( handleAuthStateChange );
-const handleAuthStateChange = (user) => {
-    try{
-        if( user.customClaims.access ){
-            location.reload();
-            console.log(`onAuthStateChanged { ${JSON.stringify(user.customClaims)} }`);
+
+signOutForm.addEventListener("submit", (e)=>{
+    e.preventDefault();
+    const ip = 'localhost'
+
+    const baseUrl = `http://${ip}:3000/user/`;
+    fetch(baseUrl + "sign-out", {
+        method: "POST",
+    })
+    .then( response => {
+
+        if( response.status === 200 || response.status === 400){ //si es 201 redireccionar
+            window.location.href = baseUrl + 'login';
+        }else{//sino, quedarse en la vista de sign up y mostrar mensaje de error
+        
         }
-    }catch( error ){
-
-    }
-    unsubscribe();
-}
-
-// const noVis = document.getElementById("noVis");
-
-// const accAttribute = noVis.getAttribute("accAttribute");
-
-// const pollAccess = () => {
-//     const baseUrl = 'http://localhost:3000/user/';
-//     fetch( baseUrl + 'check-access')
-// }
+    });
+});
+// // module.exports = { authFront };
