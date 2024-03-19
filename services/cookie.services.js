a80c7e193b37749e0c48d48af8530ca88baaf5d3
Antonio Alvarez Mayoral <aalvarez59@ucol.mx>
2024-03-19 10:09:12 -0600

    encriptacion de la cookie

--- services/cookie.services.js
+++ services/cookie.services.js
@@ -1,4 +1,8 @@
 const cookie = require('cookie');
+const AES = require('crypto-js/aes');
+const enc = require('crypto-js/enc-utf8');
+const SHA256 = require('crypto-js/sha256');
+const { S_KEY } = require('../helpers/config');
 
 class CookieService {
     
@@ -9,18 +13,25 @@ class CookieService {
         const cookieOptions = {
             secure: secureCookie,
             httpOnly: httpOnlyCookie,
-            maxAge: 60 * 60 * 2,
+            maxAge: 60 * 60 * 2 * 1000,
             sameSite: 'strict',
             path: '/'
         };
-        const cookieString = cookie.serialize('jwtToken', jwtToken, cookieOptions);
-        return cookieString;
+        // const cookieString = cookie.serialize('jwtToken', jwtToken, cookieOptions);
+        //encrypt jwttoken before setting it as cookie
+        const cryptedCookie = AES.encrypt(jwtToken, S_KEY).toString();
+
+        return [cryptedCookie,cookieOptions];
     }
     parseCookie(headersCookie){
         try{
             const cookies = cookie.parse(headersCookie || '');
             const cookieToken = cookies.jwtToken;
-            return cookieToken;
+
+            const bytes = AES.decrypt(cookieToken, S_KEY);
+            const cookieString = bytes.toString(enc);
+
+            return cookieString;
         }catch( error ){
             return '';
         }
