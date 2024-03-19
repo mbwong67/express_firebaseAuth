00b8e2d47ea6941f53a953c78bcc4959f4d3b28f
Antonio Alvarez Mayoral <aalvarez59@ucol.mx>
2024-03-19 10:10:33 -0600

    eliminacion de comentarios y ajuste de metodos

--- services/user.services.js
+++ services/user.services.js
@@ -5,23 +5,13 @@ const { createUserWithEmailAndPassword, signInWithEmailAndPassword, signOut } = 
 const { sortManager } = require('./sort.services');
 
 class UserService {
-   #userCredentials = [];
-   #cou
 
-    addUserCredential(userObj, userTkn){
-      if(this.#userCredentials.length === 0){
-        this.#userCredentials[0] = [userObj, userTkn]
-      }else{
-        // let pos = sortManager.binarySearch(userObj);
-      }
-    }
     async register(email, password) {
       try{
         //user is created for Firebase Auth and returns user obj
         const userCredential = await createUserWithEmailAndPassword(auth, email, password);
         const user = userCredential.user;
         //sets claims that are applied into the user jwt token
-        //TODO: probar que funce
         const setClaims = await authAdmin.setCustomUserClaims(user.uid, { 
           ...user.claims,
           admin: false,
@@ -42,7 +32,6 @@ class UserService {
         const user = userCredential.user;
 
         //gets user token jwt
-        //TODO: checar si jala
         const idToken = await user.getIdTokenResult();
         //Confirm is a common user
         //TODO: este codigo permite que cada que se logea, no se tiene acceso
@@ -57,7 +46,6 @@ class UserService {
         //   });
         // }
         const jwtToken = await user.getIdToken(true);
-        // console.log(`authenticate {${JSON.stringify(idToken.claims)}}`);
         return jwtToken;
 
       }catch( error ){
@@ -66,32 +54,14 @@ class UserService {
       }
     }
 
-    //TODO: Probar que funce
     async signOut() {
       try{
-        //gets user token jwt
-        const idToken = await auth.currentUser.getIdTokenResult();
-        //Confirm is a common user
-        //TODO: checar si es mejor que este en el login
-        if(!idToken.claims.admin){
-          const uid = auth.currentUser.uid;
-          //sets access to default (false)
-        }
-
         const signOutFunction = await signOut(auth);
-
         return 1
+
       }catch( error ){
         console.error( error.message );
         return 0
-      }
-    }
-    async verifyToken(token){
-      try{
-        const idToken = await authAdmin.verifyIdToken(token);
-        return idToken;
-      }catch( error ){
-        return '';
       }
     }
 };
